19f7c675239f9417f6850b03f6356d1df5f94464
fabia <fabia@172.18.38.122>
2018-12-17 09:54:23 +0100

    v1.1

--- src/SittingDucks/DuckBot3.java
+++ src/SittingDucks/DuckBot3.java
@@ -15,7 +15,7 @@ public class DuckBot3 extends TeamRobot
     ScannedRobots targetedEnemy;
     double firepower = 1, midpointPower = 250, movingOffset = 0;
     boolean aggressiveMode = false;
-    int counter = 0;
+    int counter = 0; //LALALALALALA
 
     @Override
     public void run()
